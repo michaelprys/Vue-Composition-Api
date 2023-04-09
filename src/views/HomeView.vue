07705301d9e4ac956fe066da28a29300c1e5e20d
michaelprys <musha09cz@gmail.com>
2023-04-10 00:18:13 +0200

    methods, computed, watch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,15 @@
     <h3>{{ counterData.title }}:</h3>
 
     <div>
-      <button @click="decreaseCounter" class="btn">-</button>
+      <button @click="decreaseCounter(2)" class="btn">--</button>
+      <button @click="decreaseCounter(1)" class="btn">-</button>
+
       <span class="counter">{{ counterData.count }}</span>
-      <button @click="increaseCounter" class="btn">+</button>
+      <button @click="increaseCounter(1, $event)" class="btn">+</button>
+      <button @click="increaseCounter(2)" class="btn">++</button>
     </div>
+
+    <p>This counter is {{ oddOrEven }}</p>
 
     <div class="edit">
       <h4>Edit counter title:</h4>
@@ -17,7 +22,7 @@
 </template>
 
 <script setup>
-import { reactive } from "vue";
+import { reactive, computed, watch } from "vue";
 
 const appTitle = "My Amazing Counter App";
 
@@ -26,13 +31,48 @@ const counterData = reactive({
   title: "My Counter",
 });
 
-const increaseCounter = () => {
-  counterData.count++;
+watch(
+  () => counterData.count,
+  (newCount) => {
+    if (newCount === 20) {
+      alert("You made it to 20!");
+    }
+  }
+);
+
+const oddOrEven = computed(() => {
+  if (counterData.count % 2 === 0) return "even";
+  return "odd";
+});
+
+const increaseCounter = (amount, e) => {
+  counterData.count += amount;
 };
-const decreaseCounter = () => {
-  counterData.count--;
+const decreaseCounter = (amount) => {
+  counterData.count -= amount;
 };
 </script>
+<!--
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  computed: {
+    myComputedProperty() {
+      // perform some logic based on a data property
+      return "my result";
+    },
+  },
+  watch: {
+    count(newCount, oldCount) {
+      if (newCount == 20) alert("asfadfsas");
+    },
+  },
+};
+</script> -->
 
 <style scoped>
 .home {
