83229d78d714a804db445cdea9c8d67c39022baa
michaelprys <musha09cz@gmail.com>
2023-04-09 17:32:28 +0200

    reactive object

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,69 +1,38 @@
 <template>
   <div class="home">
+    <h3>{{ counterData.title }}:</h3>
+
     <div>
       <button @click="decreaseCounter" class="btn">-</button>
-      <span class="counter">{{ counter }}</span>
+      <span class="counter">{{ counterData.count }}</span>
       <button @click="increaseCounter" class="btn">+</button>
+    </div>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input v-model="counterData.title" type="text" />
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { reactive } from "vue";
 
-const counter = ref(0);
+// const counter = ref(0),
+//   counterTitle = ref("My Counter");
+
+const counterData = reactive({
+  count: 0,
+  title: "My Counter",
+});
 
 const increaseCounter = () => {
-  counter.value++;
+  counterData.count++;
 };
 const decreaseCounter = () => {
-  counter.value--;
+  counterData.count--;
 };
 </script>
-
-<!--
-<script>
-import { ref } from "vue";
-
-export default {
-  setup() {
-    const counter = ref(0);
-
-    const increaseCounter = () => {
-      counter.value++;
-    };
-    const decreaseCounter = () => {
-      counter.value--;
-    };
-    return {
-      counter,
-      increaseCounter,
-      decreaseCounter,
-    };
-  },
-};
-</script>
--->
-
-<!--
-<script>
-export default {
-  data() {
-    return {
-      counter: 0,
-    };
-  },
-  methods: {
-    increaseCounter() {
-      this.counter++;
-    },
-    decreaseCounter() {
-      this.counter--;
-    },
-  },
-};
-</script>
--->
 
 <style scoped>
 .home {
@@ -75,5 +44,8 @@ export default {
 .counter {
   font-size: 40px;
   margin: 10px;
+}
+.edit {
+  margin-top: 60px;
 }
 </style>
