e83a07afe09b801341fdcd9b47a214a2049d8da9
michaelprys <musha09cz@gmail.com>
2023-04-12 22:32:42 +0200

    slots

---
+++ src/components/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+  <teleport to=".modals-container">
+    <div class="modal">
+      <h1><slot name="title" /></h1>
+      <slot />
+      <button>Hide modal</button>
+    </div>
+  </teleport>
+</template>
+
+<style scoped>
+.modal {
+  background: beige;
+  color: black;
+  padding: 10px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,38 +2,22 @@
   <div class="modals">
     <h1>Modals</h1>
     <button @click="showModal = true">Show modal</button>
-    <teleport to=".modals-container">
-      <div class="modal" v-if="showModal">
-        <h1>This is a modal</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-          nostrum, eum quibusdam harum soluta illum cupiditate culpa aut
-          mollitia laboriosam, quod quo fuga quidem amet ipsum dolore
-          consequuntur voluptatem dolorem!
-        </p>
-        <button @click="showModal = false">Hide modal</button>
-      </div>
-    </teleport>
+    <Modal v-if="showModal">
+      <template #title>My new title</template>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        nostrum, eum quibusdam harum soluta illum cupiditate culpa aut mollitia
+        laboriosam, quod quo fuga quidem amet ipsum dolore consequuntur
+        voluptatem dolorem!
+      </p></Modal
+    >
   </div>
 </template>
 
 <script setup>
 /*imports*/
 import { ref } from "vue";
+import Modal from "../components/Modal.vue";
 /*modals*/
 const showModal = ref(false);
 </script>
-
-<style scoped>
-.modal {
-  background: beige;
-  color: black;
-  padding: 10px;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1;
-}
-</style>
